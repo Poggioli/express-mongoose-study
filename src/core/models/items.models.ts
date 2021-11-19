697f8bfcf98259154961f7f168b245458d36553a
João Pogiolli <joaopogiolli@gmail.com>
2021-11-19 09:21:26 -0300

    creation of items model

---
+++ src/core/models/items.models.ts
@@ -0,0 +1,39 @@
+import {
+  Document, model, ObjectId, Schema
+} from 'mongoose'
+
+export interface ItemInterface extends Document {
+  name: string,
+  price: number,
+  description: string,
+  updatedAt: Date,
+  createdAt: Date,
+  _id: ObjectId
+}
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 300
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+const Item = model<ItemInterface>('Item', itemSchema)
+export default Item
