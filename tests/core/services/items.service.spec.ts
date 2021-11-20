34633d9aa9c29043e3da0f30842f30cfbbf560c1
João Pogiolli <joaopogiolli@gmail.com>
2021-11-20 11:25:46 -0300

    Add items service

--- tests/core/services/items.service.spec.ts
+++ tests/core/services/items.service.spec.ts
@@ -1,20 +1,18 @@
+/* eslint-disable dot-notation */
 import ItemsService from '../../../src/core/services'
-import ItemsRepository from '../../../src/core/repositories'
 import { Item } from '../../../src/core/models'
 
 describe('ItemsService', () => {
   let service: ItemsService
-  let repository: ItemsRepository
 
   beforeEach(() => {
     service = new ItemsService()
-    repository = new ItemsRepository()
   })
 
   describe('FindAll method', () => {
     it(`Should return a Item[]
         When call findAll`, async () => {
-      jest.spyOn(repository, 'findAll').mockResolvedValueOnce([])
+      jest.spyOn(service['repository'], 'findAll').mockResolvedValueOnce([])
       const result: Item[] = await service.findAll()
       expect(result).toStrictEqual([])
     })
