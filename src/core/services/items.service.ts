271989222ce74325dc82cd7f9b70eeae771ab321
João Pogiolli <joaopogiolli@gmail.com>
2021-11-20 12:03:33 -0300

    Add tests to findAll Method in items services

--- src/core/services/items.service.ts
+++ src/core/services/items.service.ts
@@ -1,5 +1,6 @@
 import ItemsRepository from '@repositories'
-import { Item } from '@models'
+import { Item } from '@src/core/models'
+import { Request, Response } from 'express'
 
 export default class ItemsService {
   private repository: ItemsRepository
@@ -8,5 +9,9 @@ export default class ItemsService {
     this.repository = new ItemsRepository()
   }
 
-  public findAll = async (): Promise<Item[]> => this.repository.findAll()
+  public async findAll(req: Request, resp: Response): Promise<void | Response> {
+    return this.repository.findAll()
+      .then((items: Item[]) => resp.status(200).json(items))
+      .catch((err) => { resp.status(500).json(err.message) })
+  }
 }
--- tests/core/services/items.service.spec.ts
+++ tests/core/services/items.service.spec.ts
@@ -1,20 +1,53 @@
 /* eslint-disable dot-notation */
+import { Request, Response } from 'express'
 import ItemsService from '../../../src/core/services'
-import { Item } from '../../../src/core/models'
+import ItemsRepository from '../../../src/core/repositories'
 
+const service = Object.getPrototypeOf(new ItemsService())
+// const service = new ItemsService()
 describe('ItemsService', () => {
-  let service: ItemsService
+  let request: Partial<Request>
+  let response: Partial<Response>
+  let spyResponseJson: jest.SpyInstance
+  let spyResponseStatus: jest.SpyInstance
+  let repository: ItemsRepository
 
   beforeEach(() => {
-    service = new ItemsService()
+    response = {
+      status: () => response as Response,
+      json: jest.fn()
+    }
+    request = {}
+    spyResponseStatus = jest.spyOn(response, 'status')
+    spyResponseJson = jest.spyOn(response, 'json')
+    repository = new ItemsRepository()
+    service.repository = repository
   })
 
   describe('FindAll method', () => {
     it(`Should return a Item[]
+        And status code 200
         When call findAll`, async () => {
-      jest.spyOn(service['repository'], 'findAll').mockResolvedValueOnce([])
-      const result: Item[] = await service.findAll()
-      expect(result).toStrictEqual([])
+      expect.assertions(4)
+      jest.spyOn(repository, 'findAll').mockResolvedValueOnce([])
+      await service.findAll(request as Request, response as Response)
+      expect(spyResponseStatus).toHaveBeenCalledTimes(1)
+      expect(spyResponseStatus).toHaveBeenCalledWith(200)
+      expect(spyResponseJson).toHaveBeenCalledTimes(1)
+      expect(spyResponseJson).toHaveBeenCalledWith([])
+    })
+
+    it(`Should return an error message
+        And status code 500
+        When call findAll`, async () => {
+      jest.spyOn(repository, 'findAll').mockRejectedValueOnce({ message: 'Error message' })
+      expect.assertions(4)
+      await service
+        .findAll(request as Request, response as Response)
+      expect(spyResponseStatus).toHaveBeenCalledTimes(1)
+      expect(spyResponseStatus).toHaveBeenCalledWith(500)
+      expect(spyResponseJson).toHaveBeenCalledTimes(1)
+      expect(spyResponseJson).toHaveBeenCalledWith('Error message')
     })
   })
 })
