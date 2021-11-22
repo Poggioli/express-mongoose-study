5cf4566efd56ae2a3fe214f18f088c0ee54933b7
João Pogiolli <joaopogiolli@gmail.com>
2021-11-22 14:08:29 -0300

    Add exportation to index

--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -1,3 +1,9 @@
 import ItemModel, { Item } from './items.model'
+import UserModel, { User } from './users.model'
 
-export { ItemModel, Item }
+export {
+  ItemModel,
+  Item,
+  UserModel,
+  User
+}
