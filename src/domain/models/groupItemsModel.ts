728327034888971caedd37a9996cf171276ba9ac
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2021-12-06 09:15:25 -0300

    Add model to groupItem

---
+++ src/domain/models/groupItemsModel.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from 'mongoose'
+import { Item } from './itemsModel'
+
+export interface GroupItem extends Document {
+  name: string,
+  description: string,
+  items: mongoose.Types.ObjectId[] | Item[]
+  active?: boolean,
+  createdAt: Date,
+  updatedAt?: Date
+}
+
+const groupItemSchema = new Schema<GroupItem>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  items: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item'
+    }],
+    validate: [(v: any[]) => Array.isArray(v) && v.length > 0, '{PATH} is shorter than the minimum allowed length (1).']
+  }
+}, {
+  timestamps: true
+})
+
+const GroupItemModel = mongoose.model<GroupItem>('GroupItem', groupItemSchema)
+export default GroupItemModel
